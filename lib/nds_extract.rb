ba7f8ca19b8d419c3e251da8022a2cffb217e9cb
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-11 01:40:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,8 +13,10 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  result ={}
   totals = director_data[:worldwide_gross]
   if money in totals do 
+    result[]
     
   
   #pp director_data
